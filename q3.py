fdb592010a8e00da81c74b70b35fce8575d636e1
Tal Barami <baramit@post.bgu.ac.il>
2019-07-28 21:26:28 +0300

    Output results to file.

--- q3.py
+++ q3.py
@@ -6,16 +6,13 @@ from test_utils import plot_sgd_results
 import scipy.io as sio
 
 
-def test_mnist():
+def test_mnist(iterations, learning_rate, batch_size):
     trainX, trainY, testX, testY = load_mnist()
 
     labels_count = trainY.shape[0]
     sample_size, sample_count = trainX.shape
 
-    iterations = 100
-    batch_size = 100
     freq = 1
-    learning_rate = 0.03
 
     w = np.random.randn(sample_size, labels_count)
     b = np.random.randn(labels_count, 1)
@@ -24,12 +21,40 @@ def test_mnist():
 
     model = linear_model(theta, batch_size, learning_rate, iterations, freq)
     model.train(trainX, trainY, testX, testY)
+    plot_sgd_results(model, 'SGD results (MNIST)', 'q3_mnist_data', False)
+
     iterations, train_loss, train_accuracy, test_loss, test_accuracy = zip(*model.training_records)
-    plot_sgd_results(iterations, train_loss, train_accuracy, test_loss, test_accuracy,
-                     'SGD results (MNIST)', 'q3_mnist_data', False)
+    return train_loss[-1], train_accuracy[-1], test_loss[-1], test_accuracy[1]
 
 
-def test_PeaksData():
+def test_GMMData(iterations, learning_rate, batch_size):
+    data = sio.loadmat('resources/GMMData.mat')
+    trainX = data['Yt']
+    trainY = data['Ct']
+    testX = data['Yv']
+    testY = data['Cv']
+
+    labels_count = trainY.shape[0]
+    sample_size, sample_count = trainX.shape
+
+    freq = 1
+
+    w = np.random.randn(sample_size, labels_count)
+    b = np.random.randn(labels_count, 1)
+    theta = np.concatenate((b.flatten('F'), w.flatten('F'))) \
+        .reshape(b.shape[0] * b.shape[1] + w.shape[0] * w.shape[1], 1)
+
+    model = linear_model(theta, batch_size, learning_rate, iterations, freq)
+    model.train(trainX, trainY, testX, testY)
+
+    plot_sgd_results(model, f'SGD results (GMMData)',
+                     f'q3_gmm_data_{iterations}_{learning_rate}_{batch_size}', False)
+
+    iterations, train_loss, train_accuracy, test_loss, test_accuracy = zip(*model.training_records)
+    return train_loss[-1], train_accuracy[-1], test_loss[-1], test_accuracy[1]
+
+
+def test_PeaksData(iterations, learning_rate, batch_size):
     data = sio.loadmat('resources/PeaksData.mat')
     trainX = data['Yt']
     trainY = data['Ct']
@@ -39,10 +64,7 @@ def test_PeaksData():
     labels_count = trainY.shape[0]
     sample_size, sample_count = trainX.shape
 
-    iterations = 100
-    batch_size = 100
     freq = 1
-    learning_rate = 0.03
 
     w = np.random.randn(sample_size, labels_count)
     b = np.random.randn(labels_count, 1)
@@ -51,11 +73,70 @@ def test_PeaksData():
 
     model = linear_model(theta, batch_size, learning_rate, iterations, freq)
     model.train(trainX, trainY, testX, testY)
+
+    plot_sgd_results(model, 'SGD results (PeaksData)',
+                     f'q3_peaks_data_{iterations}_{learning_rate}_{batch_size}', False)
+
     iterations, train_loss, train_accuracy, test_loss, test_accuracy = zip(*model.training_records)
-    plot_sgd_results(iterations, train_loss, train_accuracy, test_loss, test_accuracy,
-                     'SGD results (PeaksData)', 'q3_peaks_data', False)
+    return train_loss[-1], train_accuracy[-1], test_loss[-1], test_accuracy[1]
+
+
+
+def test_SwissRollData(iterations, learning_rate, batch_size):
+    data = sio.loadmat('resources/SwissRollData.mat')
+    trainX = data['Yt']
+    trainY = data['Ct']
+    testX = data['Yv']
+    testY = data['Cv']
+
+    labels_count = trainY.shape[0]
+    sample_size, sample_count = trainX.shape
+
+    freq = 1
+
+    w = np.random.randn(sample_size, labels_count)
+    b = np.random.randn(labels_count, 1)
+    theta = np.concatenate((b.flatten('F'), w.flatten('F'))) \
+        .reshape(b.shape[0] * b.shape[1] + w.shape[0] * w.shape[1], 1)
+
+    model = linear_model(theta, batch_size, learning_rate, iterations, freq)
+    model.train(trainX, trainY, testX, testY)
+
+    plot_sgd_results(model, 'SGD results (SwissRollData)',
+                     f'q3_swiss_roll_data_{iterations}_{learning_rate}_{batch_size}', False)
+
+    iterations, train_loss, train_accuracy, test_loss, test_accuracy = zip(*model.training_records)
+    return train_loss[-1], train_accuracy[-1], test_loss[-1], test_accuracy[1]
 
 
 if __name__ == "__main__":
-    test_mnist()
-    test_PeaksData()
+    iterations = [100, 200]
+    learning_rates = [0.3, 0.03, 0.003]
+    batch_sizes = [50, 100]
+
+    gmm = []
+    peaks = []
+    swiss = []
+
+    for i in iterations:
+        for l in learning_rates:
+            for b in batch_sizes:
+                gmm.append((i, l, b, test_GMMData(i, l, b)))
+                peaks.append((i, l, b, test_PeaksData(i, l, b)))
+                swiss.append((i, l, b, test_SwissRollData(i, l, b)))
+
+    with open('q3_gmm_data.txt', 'a') as file:
+        file.truncate(0)
+        for s in [f'{i} & {l} & {b} & {train_loss} & {train_accuracy} & {test_loss} & {test_accuracy} \\\\\n' for (i, l, b, (train_loss, train_accuracy, test_loss, test_accuracy)) in gmm]:
+            file.write(s)
+
+    with open('q3_peaks_data.txt', 'a') as file:
+        file.truncate(0)
+        for s in [f'{i} & {l} & {b} & {train_loss} & {train_accuracy} & {test_loss} & {test_accuracy} \\\\\n' for (i, l, b, (train_loss, train_accuracy, test_loss, test_accuracy)) in peaks]:
+            file.write(s)
+
+    with open('q3_swiss_roll_data.txt', 'a') as file:
+        file.truncate(0)
+        for s in [f'{i} & {l} & {b} & {train_loss} & {train_accuracy} & {test_loss} & {test_accuracy} \\\\\n' for (i, l, b, (train_loss, train_accuracy, test_loss, test_accuracy)) in swiss]:
+            file.write(s)
+
--- runner.py
+++ runner.py
@@ -6,8 +6,8 @@ from test_utils import compare_sgd_result
 from neural_network import rnn_model
 
 
-def testGMMData():
-    print('Neural network is running on GMM dataset')
+def testGMMData(iterations, learning_rate, batch_size, layers_count):
+    print(f'Neural network is running on GMM dataset with {iterations}, {learning_rate}, {batch_size}, {layers_count}')
     data = sio.loadmat('resources/GMMData.mat')
     trainX = data['Yt']
     trainY = data['Ct']
@@ -17,10 +17,6 @@ def testGMMData():
     labels_count = trainY.shape[0]
     sample_size = trainX.shape[0]
 
-    layers_count = 5
-    batch_size = 100
-    learning_rate = 0.03
-    iterations = 100
     freq = 1
 
     theta_layer_size = sample_size + 2 * (sample_size ** 2)
@@ -31,16 +27,20 @@ def testGMMData():
     model = rnn_model(theta, layers_count, batch_size, learning_rate, iterations, freq)
     model.train(trainX, trainY, testX, testY)
 
-    plot_sgd_results(model, 'SGD results (GMM Data)', f'q5_gmm_data', False)
+    plot_sgd_results(model, 'SGD results (GMM Data)',
+                     f'q5_gmm_data_{iterations}_{learning_rate}_{batch_size}_{layers_count}', False)
 
     model_momentum = rnn_model(theta, layers_count, batch_size, learning_rate, iterations, freq, gamma=0.5)
     model_momentum.train(trainX, trainY, testX, testY)
     compare_sgd_result([model, model_momentum], 'SGD with / without momentum (GMM Data)',
-                       'q5_gmm_data_momentum', False)
+                       f'q5_gmm_data_momentum_{iterations}_{learning_rate}_{batch_size}_{layers_count}', False)
+
+    iterations, train_loss, train_accuracy, test_loss, test_accuracy = zip(*model.training_records)
+    return train_loss[-1], train_accuracy[-1], test_loss[-1], test_accuracy[-1]
 
 
-def testPeaksData():
-    print('Neural network is running on Peaks dataset')
+def testPeaksData(iterations, learning_rate, batch_size, layers_count):
+    print(f'Neural network is running on Peaks dataset with {iterations}, {learning_rate}, {batch_size}, {layers_count}')
     data = sio.loadmat('resources/PeaksData.mat')
     trainX = data['Yt']
     trainY = data['Ct']
@@ -50,10 +50,6 @@ def testPeaksData():
     labels_count = trainY.shape[0]
     sample_size = trainX.shape[0]
 
-    layers_count = 7
-    batch_size = 100
-    learning_rate = 0.03
-    iterations = 200
     freq = 1
 
     theta_layer_size = sample_size + 2 * (sample_size ** 2)
@@ -65,16 +61,19 @@ def testPeaksData():
     model.train(trainX, trainY, testX, testY)
 
     plot_sgd_results(model, 'SGD results (Peaks Data)',
-                     f'q5_peaks_data', False)
+                     f'q5_peaks_data_{iterations}_{learning_rate}_{batch_size}_{layers_count}', False)
 
     model_momentum = rnn_model(theta, layers_count, batch_size, learning_rate, iterations, freq, gamma=0.5)
     model_momentum.train(trainX, trainY, testX, testY)
     compare_sgd_result([model, model_momentum], 'SGD with / without momentum (Peaks Data)',
-                       'q5_peaks_data_momentum', False)
+                       f'q5_peaks_data_momentum_{iterations}_{learning_rate}_{batch_size}_{layers_count}', False)
+
+    iterations, train_loss, train_accuracy, test_loss, test_accuracy = zip(*model.training_records)
+    return train_loss[-1], train_accuracy[-1], test_loss[-1], test_accuracy[-1]
 
 
-def testSwissRollData():
-    print('Neural network is running on SwissRoll dataset')
+def testSwissRollData(iterations, learning_rate, batch_size, layers_count):
+    print(f'Neural network is running on SwissRoll dataset with {iterations}, {learning_rate}, {batch_size}, {layers_count}')
     data = sio.loadmat('resources/SwissRollData.mat')
     trainX = data['Yt']
     trainY = data['Ct']
@@ -84,10 +83,6 @@ def testSwissRollData():
     labels_count = trainY.shape[0]
     sample_size = trainX.shape[0]
 
-    layers_count = 9
-    batch_size = 100
-    learning_rate = 0.03
-    iterations = 300
     freq = 1
 
     theta_layer_size = sample_size + 2 * (sample_size ** 2)
@@ -99,14 +94,51 @@ def testSwissRollData():
     model.train(trainX, trainY, testX, testY)
 
     plot_sgd_results(model, 'SGD results (Swiss Roll Data)',
-                     f'q5_swiss_roll_data', False)
+                     f'q5_swiss_roll_data_{iterations}_{learning_rate}_{batch_size}_{layers_count}', False)
 
     model_momentum = rnn_model(theta, layers_count, batch_size, learning_rate, iterations, freq, gamma=0.5)
     model_momentum.train(trainX, trainY, testX, testY)
-    compare_sgd_result([model, model_momentum], 'SGD with / without momentum (Swiss Roll Data)', 'q5_swiss_roll_data_momentum', False)
+    compare_sgd_result([model, model_momentum], 'SGD with / without momentum (Swiss Roll Data)',
+                       f'q5_swiss_roll_data_momentum_{iterations}_{learning_rate}_{batch_size}_{layers_count}', False)
+
+    iterations, train_loss, train_accuracy, test_loss, test_accuracy = zip(*model.training_records)
+    return train_loss[-1], train_accuracy[-1], test_loss[-1], test_accuracy[-1]
 
 
 if __name__ == "__main__":
-    testGMMData()
-    testPeaksData()
-    testSwissRollData()
+    iterations = [100, 200]
+    learning_rates = [0.03, 0.003]
+    batch_sizes = [50, 100]
+    layers = [5, 7, 9]
+
+    gmm = []
+    peaks = []
+    swiss = []
+
+    for i in iterations:
+        for r in learning_rates:
+            for b in batch_sizes:
+                for l in layers:
+                    # gmm.append((i, r, b, l, testGMMData(i, r, b, l)))
+                    peaks.append((i, r, b, l, testPeaksData(i, r, b, l)))
+                    swiss.append((i, r, b, l, testSwissRollData(i, r, b, l)))
+
+    with open('q5_gmm_data.txt', 'a') as file:
+        file.truncate(0)
+        for s in [f'{i} & {r} & {b} & {l} & {np.round(train_loss, 4)} & {np.round(train_accuracy, 4)} & {np.round(test_loss, 4)} & {np.round(test_accuracy, 4)} \\\\\n'
+                  for (i, r, b, l, (train_loss, train_accuracy, test_loss, test_accuracy)) in gmm]:
+            file.write(s)
+
+    with open('q5_peaks_data.txt', 'a') as file:
+        file.truncate(0)
+        for s in [
+            f'{i} & {r} & {b} & {l} & {np.round(train_loss, 4)} & {np.round(train_accuracy, 4)} & {np.round(test_loss, 4)} & {np.round(test_accuracy, 4)} \\\\\n'
+                for (i, r, b, l, (train_loss, train_accuracy, test_loss, test_accuracy)) in peaks]:
+            file.write(s)
+
+    with open('q5_swiss_roll_data.txt', 'a') as file:
+        file.truncate(0)
+        for s in [
+            f'{i} & {r} & {b} & {l} & {np.round(train_loss, 4)} & {np.round(train_accuracy, 4)} & {np.round(test_loss, 4)} & {np.round(test_accuracy, 4)} \\\\\n'
+                for (i, r, b, l, (train_loss, train_accuracy, test_loss, test_accuracy)) in swiss]:
+            file.write(s)
